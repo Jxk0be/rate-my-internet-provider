31d23dccebab71d1030c2ca5aefc76dfac4381c8
Jxk0be <jakeshoffner27@gmail.com>
2025-04-27 23:55:44 -0400

    Integrate TailwindCSS and clean up styles
    Replaced custom CSS with TailwindCSS integration for improved codestyle consistency and flexibility. Updated dependencies to include TailwindCSS and associated packages, removing unused styling and associated imports in the codebase.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class=" text-3xl font-bold text-red-500">Testing TailwindCSS</h1>
   </main>
 </template>
