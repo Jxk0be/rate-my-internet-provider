38f3bad3acff6c0013cd87112180bf0d606b5b9b
Jxk0be <jakeshoffner27@gmail.com>
2025-04-28 21:12:53 -0400

    Setting up basics of darkMode & PrimeVue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+const testing = () => {
+  document.documentElement.classList.toggle('my-app-dark')
+}
 </script>
 
 <template>
-  <div class="w-full bg-amber-400 flex justify-center">
-    <div class="max-w-[1280px] w-full bg-red-500">
+  <div class="w-full h-[100vh] flex justify-center">
+    <div class="max-w-[1280px] w-full">
+      <AppButton @click="testing" label="submit" />
       <RouterView />
     </div>
   </div>
 </template>
-
