b2d122a2f821c50a2bf738236555304fd1eeabaf
Jxk0be <jakeshoffner27@gmail.com>
2025-05-25 03:47:13 -0400

    Fixing build errors

--- src/pages/[location]/[provider]/add-review/index.vue
+++ src/pages/[location]/[provider]/add-review/index.vue
@@ -1,26 +1,4 @@
-<script setup lang="ts">
-import { useRoute, useRouter } from 'vue-router'
-import { computed, ref, watch } from 'vue'
-import { supabase } from '@/supabase.ts'
-import type { Provider, Location } from '@/types/supabaseTables'
-
-const route = useRoute()
-const router = useRouter()
-const isLoading = ref(false)
-const isRouting = ref(false)
-const currentLocation = ref<Location | null>(null)
-const locationProviders = ref<Provider[]>([])
-
-const location = computed(() => route.params.addReview)
-const locationName = computed(() => {
-  if (!location.value) return ''
-
-  const [cityPart, state] = location.value.toString().split('_')
-  const city = cityPart.replace(/([A-Z])/g, ' $1').trim()
-
-  return `${city}, ${state}`
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
